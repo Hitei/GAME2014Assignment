6e6532b09b345457ca79001ba52c8a5a9d27fb31
DaBoss1234 <ardainan196@gmail.com>
2016-12-06 00:10:39 -0500

    Made main menu and changed starting screen

--- app/src/main/java/com/overbudget/game2014/MainMenu.java
+++ app/src/main/java/com/overbudget/game2014/MainMenu.java
@@ -1,7 +1,9 @@
 package com.overbudget.game2014;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        TextView text = (TextView) findViewById(R.id.textView2);
+        TextView text2 = (TextView) findViewById(R.id.textView2);
+        TextView text3 = (TextView) findViewById(R.id.textView3);
+        text.setTextColor(Color.RED);
+        text2.setTextColor(Color.RED);
+        text3.setTextColor(Color.RED);
+        //GifImageView viewImage = (GifImageView) findViewById(R.id.text);
     }
 }
